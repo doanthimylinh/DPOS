aa92b639e205d956a5a6bc4ddc3fc73228f5656d
Đoàn Kiều Mỹ Ngọc <97938927+DoanKieuMyNgoc@users.noreply.github.com>
2023-01-10 23:40:48 +0700

    10/1

---
+++ src/screens/GetStart.js
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+
+export default function GetStart() {
+    const navigation = useNavigation();
+  return (
+    <View>
+        <TouchableOpacity
+        onPress={()=>navigation.navigate("Home")}>
+        <Text>GetStart</Text>
+        </TouchableOpacity>
+      
+    </View>
+  )
+}

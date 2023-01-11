3709e34f05cc08d9f13a205f7f6bc1237c5a7e82
Doan Thi My Linh <linhdoan1122288@gmail.com>
2023-01-11 22:53:59 +0700

    update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -15,6 +16,29 @@ export default function App() {
         initialRouteName="Vertify">
         {/* <Stack.Screen name="SignIn" component={SignIn} /> */}
         <Stack.Screen name="Vertify" component={Vertify} />
+=======
+import { View, Text } from 'react-native'
+import React from 'react'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/dashboard/Home'
+import GetStart from './src/components/GetStart'
+import SignIn from './src/components/authen/SignIn'
+import Vertify from './src/components/authen/Vertify'
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="GetStart"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name='GetStart' component={GetStart} />
+        <Stack.Screen name='SignIn' component={SignIn} />
+        <Stack.Screen name='Vertify' component={Vertify} />
+        <Stack.Screen name='Home' component={Home} />
+>>>>>>> Stashed changes
       </Stack.Navigator>
     </NavigationContainer>
   );

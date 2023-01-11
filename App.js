a5945c131167a66403dd1ce524f629251c3a3e59
Doan Thi My Linh <linhdoan1122288@gmail.com>
2023-01-11 23:44:24 +0700

    lan cuoi

--- App.js
+++ App.js
@@ -1,44 +1,24 @@
-<<<<<<< Updated upstream
+import {View, Text} from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-
-import {createStackNavigator} from '@react-navigation/stack';
-import SignIn from './src/components/SignIn';
-import Vertify from './src/components/Vertify';
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="Vertify">
-        {/* <Stack.Screen name="SignIn" component={SignIn} /> */}
-        <Stack.Screen name="Vertify" component={Vertify} />
-=======
-import { View, Text } from 'react-native'
-import React from 'react'
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/components/dashboard/Home'
-import GetStart from './src/components/GetStart'
-import SignIn from './src/components/authen/SignIn'
-import Vertify from './src/components/authen/Vertify'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/components/dashboard/Home';
+import GetStart from './src/components/GetStart';
+import SignIn from './src/components/authen/SignIn';
+import Vertify from './src/components/authen/Vertify';
+import TabScreens from './src/components/dashboard/Home';
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="GetStart"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name='GetStart' component={GetStart} />
-        <Stack.Screen name='SignIn' component={SignIn} />
-        <Stack.Screen name='Vertify' component={Vertify} />
-        <Stack.Screen name='Home' component={Home} />
->>>>>>> Stashed changes
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="GetStart" component={GetStart} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Vertify" component={Vertify} />
+        <Stack.Screen name="TabScreens" component={TabScreens} />
       </Stack.Navigator>
     </NavigationContainer>
   );
